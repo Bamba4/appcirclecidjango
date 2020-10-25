77b542c0db1ec6db8db91021eda71976ab367c13
Cheikh Ahmadou Bamba DIAGNE <ahmadoubamba.diagne@finditech.com>
2020-10-25 16:12:20 +0000

    add test

--- tracks/tests.py
+++ tracks/tests.py
@@ -1,16 +1,3 @@
-import django
 from django.test import TestCase
 
-
-class SimpleTest(TestCase):
-    """Tests for the application views."""
-    
-    @classmethod
-    def setUpClass(cls):
-        django.setup()
-    
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+# Create your tests here.
